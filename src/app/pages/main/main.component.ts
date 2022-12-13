150dd45b41f2bb0f3f6ced0bd7f7ee8dce1004b5
Furát Ismail <furat.ismail@cadcom.cz>
2022-12-13 13:30:26 +0100

    observables

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Todo } from 'src/app/interfaces/todo.interface';
 import { TodosService } from 'src/app/todos.service';
 
 @Component({
@@ -6,22 +8,34 @@ import { TodosService } from 'src/app/todos.service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnDestroy {
   data: any;
+  todos$!: Observable<Array<Todo>>
+
+
+  private subscription!: Subscription;
+
   constructor(private todosService: TodosService) {
 
   }
 
   callApi() {
-    //  this.todosService.getAll().subscribe((response) => console.log(response));
-    this.todosService.getExampleObservable().subscribe((res: any) => {
-      console.log(res)
-      this.data = res
-    })
+    this.todos$ = this.todosService.getAll();
+
+    // this.subscription = this.todosService.getAll().subscribe((response) => console.log(response));
+    // this.todosService.getExampleObservable().subscribe((res: any) => {
+    //   console.log(res)
+    //   this.data = res
+    // })
   }
 
   async callApi2() {
     const response = await this.todosService.getAllAsync();
     console.log(response)
+  }
+
+  ngOnDestroy() {
+    console.log('run')
+    this.subscription.unsubscribe();
   }
 }
